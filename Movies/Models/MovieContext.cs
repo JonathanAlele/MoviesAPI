6b1c8b2d443bc2576698acc6b6ce105acb5fe28a
JonathanAlele <jonathanalele@gmail.com>
2024-04-30 00:28:06 +0200

    Changed access modifier for movie entity

--- Movies/Models/MovieContext.cs
+++ Movies/Models/MovieContext.cs
@@ -3,7 +3,7 @@
 namespace Movies.Models;
 public class MovieContext : DbContext
 {
-	DbSet<Movie> Movies { get; set; } = null!;
+	public DbSet<Movie> Movies { get; set; } = null!;
 	public MovieContext(DbContextOptions<MovieContext> options) : base(options)
 	{
 	}
