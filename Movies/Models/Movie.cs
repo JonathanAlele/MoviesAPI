eedd694f934c33060b72b91477dda77b09283815
JonathanAlele <jonathanalele@gmail.com>
2024-04-30 00:29:16 +0200

    Modified DB column type to date only and migrated changes using code-first approach

--- Movies/Models/Movie.cs
+++ Movies/Models/Movie.cs
@@ -1,8 +1,12 @@
-﻿namespace Movies.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.Models;
 public class Movie
 {
 	public int Id { get; set; }
 	public string? Title { get; set; }
 	public string? Genre { get; set; }
+
+	[DataType(DataType.Date)]
 	public DateTime ReleaseDate { get; set; }
 }
