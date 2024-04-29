5dbe6ff16ba84f8bdc0100b810319107118dde49
JonathanAlele <jonathanalele@gmail.com>
2024-04-29 23:19:36 +0200

    Added a db context class for data access Added EF Core packages

---
+++ Movies/Models/MovieContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Movies.Models;
+public class MovieContext : DbContext
+{
+	DbSet<Movie> Movies { get; set; } = null!;
+	public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+	{
+
+	}
+}
